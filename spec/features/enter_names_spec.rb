b3d82099308f2b8ff7f3bad53475c45b44c8bf45
Aniruddh <ojhaa94@gmail.com>
2021-02-19 15:16:29 +0000

    adds player attack

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,8 +1,20 @@
-require 'web_helper'
+require 'spec_helper'
+
 feature 'Enter names' do
-    @hit_points = Battle::HIT_POINTS
+   # @hit_points = Battle::HIT_POINTS # Doesn't work here!
     scenario 'submitting names' do
+				@hit_points = Battle::HIT_POINTS # Works here?
         sign_in_and_play
+				p @hit_points
+				save_and_open_page
         expect(page).to have_content "Ash vs. Gary Gary #{@hit_points} HP"
     end
+end
+
+feature 'Attacking' do
+	scenario 'attack Player 2' do
+		sign_in_and_play
+		click_link 'Attack'
+		expect(page).to have_content "Ash attacked Gary!"
+	end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,5 +1,3 @@
-
-
 def sign_in_and_play
   visit('/')
   fill_in :player_1_name, with: "Ash"
